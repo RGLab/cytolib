6ec60b043ceae233f8ff0127ef8477a3f8cc8ca3
mikejiang <wjiang2@fhcrc.org>
2017-10-05 12:03:58 -0700

    restore the change overwritten by trunk merge

--- inst/include/cytolib/spline.hpp
+++ inst/include/cytolib/spline.hpp
@@ -9,19 +9,13 @@
 #ifndef SPLINE_HPP_
 
 #define SPLINE_HPP_
-#ifdef ROUT
-#define COUT cout //TODO: originally it was Rout, to get around Rcpp, we will have to use Rprintf/printf to rewrite all the console print
-#endif
 
-
-#ifndef ROUT
-#define COUT cout
-#endif
-
+#include "global.hpp"
 #include <cmath>
 #include <stdexcept>
 #include <iostream>
-#include <valarray>
+#include <vector>
+#include <cstring>
 #define NATURAL 2
 
 using namespace std;
@@ -31,6 +25,6 @@ void spline_eval_C(int *method, int *nu, double *u, double *v,
 		 int *n, double *x, double *y, double *b, double *c, double *d);
 
 
-void natural_spline(valarray<double>x, valarray<double> y, valarray<double>& b,valarray<double>& c,valarray<double>& d);
-void spline_eval(int method, valarray<double> u, valarray<double> & v,const valarray<double> & x, const valarray<double> & y, const valarray<double> & b, const valarray<double> & c, const valarray<double> & d);
+void natural_spline(vector<double>x, vector<double> y, vector<double>& b,vector<double>& c,vector<double>& d);
+void spline_eval(int method, double* u,int nSize,const vector<double> & x, const vector<double> & y, const vector<double> & b, const vector<double> & c, const vector<double> & d);
 #endif /* SPLINE_HPP_ */
