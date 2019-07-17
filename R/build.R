bce32878106f878f783c9fa3b9d62abdbd897e2f
mikejiang <wjiang2@fhcrc.org>
2019-07-17 15:34:45 -0700

    make R package build and load

---
+++ R/build.R
@@ -0,0 +1,91 @@
+
+
+# Output the CXX flags. These flags are propagated to sourceCpp via the 
+# inlineCxxPlugin (defined below) and to packages via a line in Makevars[.win]
+# like this:
+#
+#  PKG_CXXFLAGS += $(shell "${R_HOME}/bin${R_ARCH_BIN}/Rscript.exe" -e "cytolib::CxxFlags()")
+#
+CxxFlags <- function() {
+   cat(cytolibCxxFlags())
+}
+
+
+# Output the LD flags for building against cytolib. These flags are propagated
+# to sourceCpp via the inlineCxxPlugin (defined below) and to packages 
+# via a line in Makevars[.win] like this:
+#
+#   PKG_LIBS += $(shell "${R_HOME}/bin${R_ARCH_BIN}/Rscript.exe" -e "cytolib::LdFlags()")
+#' @export
+#' @importFrom RProtoBufLib LdFlags
+#' @importFrom RcppParallel RcppParallelLibs
+#' @noRd
+LdFlags <- function(...) {
+   cat(cytolibLdFlags(...))
+}
+
+# alias for backward compatibility
+cytolibLibs <- function() {
+   LdFlags()
+}
+
+
+cytolibCxxFlags <- function() {
+   
+   flags <- c()
+   
+   
+   flags
+}
+
+# Return the linker flags requried for cytolib on this platform
+cytolibLdFlags <- function() {
+   # on Windows and Solaris we need to explicitly link against cytolib.dll
+   if ((Sys.info()['sysname'] %in% c("Windows", "SunOS")) && !isSparc()) {
+	  cytolib <- cytolibLibPath()
+	  cytoliblib <- "cytolib"
+      res <- paste("-L", asBuildPath(dirname(cytolib)), " -l", cytoliblib, sep = "")
+   } else {
+     res <- ""
+   }
+	# if(all)
+	  # res <- paste(res, gslibs)
+	res
+}
+
+# Determine the platform-specific path to the cytolib library
+cytolibLibPath <- function(suffix = "") {
+   sysname <- Sys.info()['sysname']
+   cytolibSupported <- list(
+      "Darwin" = paste("cytolib", suffix, ".dylib", sep = ""), 
+      "Linux" = paste("cytolib", suffix, ".so", sep = ""), 
+      "Windows" = paste("cytolib", suffix, ".dll", sep = ""),
+      "SunOS" = paste("cytolib", suffix, ".so", sep = "")
+   )
+   # browser()
+   if ((sysname %in% names(cytolibSupported)) && !isSparc()) {
+      libDir <- "libs/"
+      if (sysname == "Windows")
+         libDir <- paste(libDir, .Platform$r_arch, "/", sep="")
+      system.file(paste(libDir, cytolibSupported[[sysname]], sep = ""), 
+                  package = "cytolib")
+   } else {
+      NULL
+   }
+}
+
+isSparc <- function() {
+   Sys.info()['sysname'] == "SunOS" && Sys.info()[["machine"]] != "i86pc"
+}
+
+# Helper function to ape the behavior of the R build system
+# when providing paths to libraries
+asBuildPath <- function(path) {
+   if (.Platform$OS.type == "windows") {
+      path <- normalizePath(path)
+      if (grepl(' ', path, fixed=TRUE))
+         path <- utils::shortPathName(path)
+      path <- gsub("\\\\", "/", path)
+   }
+   return(path)
+}
---
+++ R/hooks.R
@@ -0,0 +1,31 @@
+
+dllInfo <- NULL
+
+.onLoad <- function(libname, pkgname) {
+   
+   # load cytolib and cytolibmalloc on supported platforms   
+   cytolib <- cytolibLibPath()
+   if (!is.null(cytolib)) {
+      if (!file.exists(cytolib)) {
+         warning(paste("cytolib library", cytolib, "not found."))
+      } else {
+         dllInfo <<- dyn.load(cytolib, local = FALSE, now = TRUE)
+      }
+   }
+  
+   
+   # load the package library
+   # library.dynam("cytolib", pkgname, libname)
+   
+}
+
+.onUnload <- function(libpath) {
+   
+   # unload the package library
+   # library.dynam.unload("cytolib", libpath)
+   
+   # unload dll if we loaded it
+  for(dll in dllInfo)
+   if (!is.null(dll))
+      dyn.unload(dll[["path"]])
+}
